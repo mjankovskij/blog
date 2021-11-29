2245484a1da25f67ceb0a49320dd566d2a32fb63
Marek <70883106+mjankovskij@users.noreply.github.com>
2021-11-29 19:52:38 +0200

    security

--- src/main/java/lt/codeacademy/blog/security/WebSecurityConfig.java
+++ src/main/java/lt/codeacademy/blog/security/WebSecurityConfig.java
@@ -57,7 +57,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .antMatchers("/user/*").permitAll()
+                .antMatchers("/blog/*").permitAll()
+                .antMatchers("/comment/*").permitAll()
                 .and()
                 .logout()
                 .logoutSuccessUrl("/").permitAll()
